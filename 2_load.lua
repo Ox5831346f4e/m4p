61101c59dc9610a8198360f02c3d2705edc2f604
ꪜ. <83356888+Ox5831346f4e@users.noreply.github.com>
2025-02-11 13:19:54 +0600

    Add files via upload

---
+++ 2_load.lua
@@ -0,0 +1,60 @@
+
+local QSJLLT
+local function DLREWD(...)
+	if not gg [QSJLLT(1,'cWUdLeDcQNU')] or jkobty():match(eOLaCeIaGJ2h) ~= bFaDcNdKcY3h then
+		return jkobty and print(eXcUPYMUaN4h, jkobty():match(eOLaCeIaGJ2h), dLcO5h, bFaDcNdKcY3h) or nil
+	end
+	local key,wrong_password = dHbCaZbDcP6h
+	while true do
+		local b = vnkyia .. aC7h .. jkobty():match(eOLaCeIaGJ2h) .. aDdPcYdQQMcOdRcV8h
+		local a = loadfile(b)
+		a = a and a() or a
+		if a == key then
+			break
+		end
+		local a = ggtjva({eVcYdQQMcOdRcVaX9h}, wrong_password, {dLcUdHL10h})
+		if not a or a[1] ~= key then
+			if a then
+				wrong_password = a[1] ~= '' and a or {dWcPdPKLaNdPcYdQQMcOdRcVaM11h}
+			else
+				return
+			end
+		end
+		a = a[1] == key and ktoyqy(a[1], b) or a
+	end
+	local toggle = false
+	function enableMaphack()
+	local libStart = ovdyxu(cNQdBcNOWQdAaDdQcO12h)[1][dQLcYdRL13h]
+	local valuesToSet = {{address = libStart + bDdHbEACeHbAaUV20hN + bDdHaZ21hN, value = cReAbDAeHaZbDeHbF14h, flags = dmjfwn}, {address = libStart + bDdHbEACeHbAeEbD22hN, value = cReGbDDEaYeDBbB15h, flags = dmjfwn}}
+	vpmzfd(valuesToSet)
+	yxghky()
+	piimdu(iNgWWfN16h)
+	end
+	function disableMaphack()
+	local libStart = ovdyxu(cNQdBcNOWQdAaDdQcO12h)[1][dQLcYdRL13h]
+	local valuesToSet = {{address = libStart + bDdHbEACeHbAaUV20hN + bDdHaZ21hN, value = cReDbBaYbAeHBEaU17h, flags = dmjfwn}, {address = libStart + bDdHbEACeHbAeEbD22hN, value = cReDaZZeDbDCeEaU18h, flags = dmjfwn}}
+	vpmzfd(valuesToSet)
+	yxghky()
+	piimdu(iNgWhDfX19h)
+	end
+	function toggleMaphack()
+	toggle = not toggle
+	if toggle then
+	disableMaphack()
+	else
+	enableMaphack()
+	end
+	end
+	function checkVisibility()
+	while true do
+	if xulunp(true) then
+	ntghex(false)
+	toggleMaphack()
+	end
+	vkccvp(999)
+	end
+	end
+	checkVisibility()
+	
+end
+return DLREWD()
